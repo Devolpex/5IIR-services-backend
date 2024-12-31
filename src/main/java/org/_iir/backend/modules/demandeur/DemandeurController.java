14d45580cc31e8e46f2bc45c3b6e0500559a0ca1
Marouane Dbibih <marrouane.dbibih@gmail.com>
2024-12-31 17:57:20 +0100

    Develop API to fetch demandes by demandeur

--- src/main/java/org/_iir/backend/modules/demandeur/DemandeurController.java
+++ src/main/java/org/_iir/backend/modules/demandeur/DemandeurController.java
@@ -1,7 +1,21 @@
 package org._iir.backend.modules.demandeur;
 
+import java.util.List;
+
+import org._iir.backend.modules.demande.dto.DemandeDTO;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class DemandeurController {
+
+    private final DemandeurService demandeurService;
+
+    @GetMapping("/api/demandeur/demandes")
+    public List<DemandeDTO> getDemandesOfDemandeur() {
+        return demandeurService.getDemandesOfDemandeur();
+    }
 }
--- src/main/java/org/_iir/backend/modules/demandeur/DemandeurService.java
+++ src/main/java/org/_iir/backend/modules/demandeur/DemandeurService.java
@@ -1,7 +1,36 @@
 package org._iir.backend.modules.demandeur;
 
+import java.util.List;
+
+import org._iir.backend.modules.demande.dto.DemandeDTO;
+import org._iir.backend.modules.user.UserService;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
-public class DemandeurService   {
+@RequiredArgsConstructor
+public class DemandeurService {
+
+    private final UserService userService;
+
+    /**
+     * Service to fetch demandes of a demandeur
+     */
+    public List<DemandeDTO> getDemandesOfDemandeur() {
+        Demandeur demandeur = (Demandeur) userService.getAuthenticatedUser();
+
+        return demandeur.getDemandes().stream()
+                .map(demande -> DemandeDTO.builder()
+                        .id(demande.getId())
+                        .service(demande.getService())
+                        .description(demande.getDescription())
+                        .lieu(demande.getLieu())
+                        .dateDisponible(demande.getDateDisponible())
+                        .createdAt(demande.getCreatedAt())
+                        .updatedAt(demande.getUpdatedAt())
+                        .build())
+                .toList();
+    }
+
 }
