6ac477c54a8853ad5efe7da86ed20ba43527f598
Marouane Dbibih <marrouane.dbibih@gmail.com>
2024-12-31 19:59:20 +0100

    add the endpoint to add and remove service to and from prestataire

--- src/main/java/org/_iir/backend/modules/prestataire/PrestataireController.java
+++ src/main/java/org/_iir/backend/modules/prestataire/PrestataireController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org._iir.backend.modules.offre.dto.OffreDTO;
 import org._iir.backend.modules.order.dto.DemandeOrderDTO;
 import org._iir.backend.modules.order.dto.OffreOrderDTO;
+import org.hibernate.mapping.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -34,5 +39,17 @@ public class PrestataireController {
     @GetMapping("/api/prestataire/orders/demandes")
     public List<DemandeOrderDTO> getDemandesOrdersByPrestataire() {
         return prestataireService.getDemandesOrdersByPrestataire();
+    }
+
+    // Endpoint to add a service to a prestataire
+    @PatchMapping("/api/prestataire/service/{id}")
+    public boolean addServiceToPrestataire(@PathVariable Long id) {
+        return prestataireService.addServiceToPrestataire(id);
+    }
+
+    // Endpoint to remove a service from a prestataire
+    @PatchMapping("/api/prestataire/service/remove/{id}")
+    public boolean removeServiceFromPrestataire(@PathVariable Long id) {
+        return prestataireService.removeServiceFromPrestataire(id);
     }
 }
---
+++ src/main/java/org/_iir/backend/modules/prestataire_services/PSService.java
@@ -0,0 +1,12 @@
+package org._iir.backend.modules.prestataire_services;
+
+import org._iir.backend.modules.user.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@org.springframework.stereotype.Service
+@RequiredArgsConstructor
+public class PSService {
+
+    
+}
